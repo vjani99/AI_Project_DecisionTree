4e6f7b990ce5b1e98327b11a2c677af9bafb3af3
drewdunk10 <ddunkelberge2018@my.fit.edu>
2021-11-04 18:41:51 -0400

    Implement DTL. TODO: Retain empty attribute types

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+from collections import defaultdict
+
 import pandas as pd
 import numpy as np
+from sys import maxsize
 
 
 # TODO: Implement DTL algorithm and sub procedures
 def DTL(examples, attributes, default):
     if examples.size == 0:
+        # print(f"No more examples left for attributes: {attributes}")
         return default
     elif all_classes_same(examples):
         # Since we know all classes are same, just return classification of first example
-        return bool(examples[0, -1])
+        # print(f"All classification are the same for: {attributes}")
+        return bool(int(examples[0, -1]))
+    elif not attributes:
+        # print(f"No attributes left for remaining examples: {examples}")
+        return mode(examples)
     else:
+        # Update the frequencies of attribute types with new set of examples
+        for a in attributes.values():
+            a.update_counts(examples[:, a.col_idx].T, get_outcomes(examples))
+
         best = choose_best_attribute(attributes, examples)
+        # print(f"Best attribute: {best.name}")
         tree = TreeNode(best.name)
+
+        # Remove best attribute from dictionary (copy)
+        subset_attributes = attributes.copy()
+        del subset_attributes[best.name]
 
         # Divide examples by the types of the best attribute.
         for val in best.types:
             ex_val_subset = get_examples_by_type(examples, val, best.col_idx)
-            sub_tree = DTL(ex_val_subset, attributes-best, mode(examples))
+            sub_tree = DTL(ex_val_subset, subset_attributes, mode(examples))
             tree.add_branch(val, sub_tree)
         return tree
 
@@ -28,12 +45,11 @@ def get_examples_by_type(examples, a_type, a_col_idx):
 
 def mode(examples):
     outcomes = get_outcomes(examples)    # Extract row of all outcomes
-    print(outcomes)
     counts = np.bincount(outcomes)       # Count occurrences of 0/1
 
     # print(f"counts {counts}")
     if counts.size != 1 and counts[0] == counts[1]:
-        print("Tie in outcomes")
+        print("Tie in outcomes, choosing False")
 
     # TODO: This ALWAYS chooses the first max calculated for tie-breaking (0)... may want to randomize
     mode = counts.argmax()
@@ -52,23 +68,30 @@ def all_classes_same(examples):
     return np.all(outcomes == outcomes[0])      # Return whether all occurrences are the same
 
 
-def choose_best_attribute(attributes, examples):
-    max_info_gain = 0
+def choose_best_attribute(att_dict, examples):
+    max_info_gain = -maxsize
     best_attribute = None
     outcomes = get_outcomes(examples)
-    for a in attributes.values():
+    # print(f'Examples: {examples}')
+    # print(f'Outcomes: {outcomes}')
+    # print(f'Remaining attributes: {[a.name for a in att_dict.values()]}')
+    for a in att_dict.values():
         curr_gain = info_gain(a, examples)
+        # print(f"{a.name} info gain: {curr_gain}")
         if curr_gain > max_info_gain:
             max_info_gain = curr_gain
             best_attribute = a
-
-        # Update the frequencies of attribute types with new set of examples
-        a.update_counts(examples[:, a.col_idx].T, outcomes)
     return best_attribute
 
 
 def info_gain(attribute, examples):
-    return goal_entropy(examples) - aggregate_entropy(attribute, examples.shape[0])
+    goal = goal_entropy(examples)
+    aggregate = aggregate_entropy(attribute, examples.shape[0])
+    # print(f'Remaining Examples: {examples.shape[0]}')
+    # print(f'Attribute {attribute.name}: {attribute.type_count}')
+    # print(f'Goal Entropy: {goal}')
+    # print(f'Aggregate Entropy: {aggregate}')
+    return goal - aggregate
 
 
 class TreeNode:
@@ -84,8 +107,9 @@ class TreeNode:
 
     def print_tree(self):
         """Print all nodes of tree (parent with children below)"""
-        print("\n" + self.name)
-        print(self.children)
+        print("\n" + self.name + "?")
+        # print(self.children)
+        print([f"{key}: Tree({c.name})" if isinstance(c, TreeNode) else f"{key}: {c}" for key, c in self.children.items()])
 
         for child in self.children.values():
             if isinstance(child, TreeNode):
@@ -94,22 +118,29 @@ class TreeNode:
 
 class Attribute():
     """Class to house details about a particular attribute as the decision tree."""
-    def __init__(self, name, outputs, outcomes, col_idx):
+    def __init__(self, name, outputs, col_idx):
         self.name = name
         self.col_idx = col_idx
 
         # Find all unique types of attribute
         self.types = np.unique(outputs)
         self.type_count = {}
+        for t in self.types:
+            self.type_count.setdefault(t, [0, 0, 0])
 
         # Set initial frequencies
-        self.update_counts(outputs, outcomes)
+        # self.update_counts(outputs, outcomes)
 
     def update_counts(self, outputs, outcomes):
         """Given a row of all outputs for this attribute and outcomes for those outputs, store frequencies of
         each output value."""
 
         # Create a dictionary of outcomes per type, defaulted to 0
+        self.types = np.unique(outputs)
+        self.type_count.clear()
+        # for t in self.types:
+            # self.type_count.setdefault(t, [0, 0, 0])
+        # self.type_count = defaultdict([0, 0, 0])
         self.type_count = {a_type: [0, 0, 0] for a_type in self.types}
         # print(f'--------------{self.name} types: {self.types}--------------\n')
         for i, a_type in enumerate(outputs):
@@ -119,6 +150,9 @@ class Attribute():
             else:
                 self.type_count[a_type][1] += 1     # Negative
             self.type_count[a_type][2] += 1         # Total
+        # for key, val in list(self.type_count.items()):
+        #     if val[2] == 0:
+        #         del self.type_count[key]
         # print(f'{self.name} frequencies [p, k, v]: {self.type_count}\n')
 
 
@@ -128,8 +162,9 @@ def goal_entropy(examples):
     counts = np.bincount(outcomes)
 
     # Calculate probability of true/false
-    prob_p = counts[1] / len(examples)
+    prob_p = counts[1] / examples.shape[0]
     prob_n = 1 - prob_p
+    # print(f"Positive prob {prob_p}, Negative prob {prob_n}")
 
     # Calculate and return goal entropy of this example subset.
     # Note: A H(goal) of 1 means that there are equal # of positive and negative examples
@@ -141,7 +176,7 @@ def aggregate_entropy(attribute, example_count):
     """Calculate the entropy values of each type of an attribute and return aggregate entropy."""
     weighted_sum = 0
     for k in attribute.types:
-        # print(f'({attribute.name}, {k}):')
+        print(f'({attribute.name}, {k}):')
         vk = attribute.type_count[k][2]
 
         prob_p = attribute.type_count[k][0] / vk
@@ -158,7 +193,7 @@ def aggregate_entropy(attribute, example_count):
 
 if __name__ == '__main__':
     # Read in examples from formatted .csv file.
-    data = pd.read_csv('data/figure_18-3.csv', sep=',', header=None).to_numpy()
+    data = pd.read_csv('data/simplified-figure_18-3.csv', sep=',', header=None).to_numpy()
 
     # Constants related to data read in.
     ATTRIBUTE_COUNT = data.shape[1] - 1
@@ -176,16 +211,15 @@ if __name__ == '__main__':
 
     EXAMPLES = np.atleast_2d(EXAMPLES)  # Ensure that a 1D array is still treated as 2D in all calculations.
 
-    mode(EXAMPLES)
-    print(all_classes_same(EXAMPLES))
+    # mode(EXAMPLES)
+    # print(all_classes_same(EXAMPLES))
 
     # # Goal entropy of ENTIRE data set
     # print(f'--------------Goal entropy of ENTIRE data set: {goal_entropy(EXAMPLES):.2f}--------------\n\n')
     #
     attributes = {}
-    init_outcomes = get_outcomes(EXAMPLES)
     for i in range(0, EXAMPLES.shape[1] - 1):
-        attributes[ATTRIBUTE_NAMES[i]] = Attribute(ATTRIBUTE_NAMES[i], EXAMPLES[:, i].T, init_outcomes, i)
+        attributes[ATTRIBUTE_NAMES[i]] = Attribute(ATTRIBUTE_NAMES[i], EXAMPLES[:, i].T, i)
     #
     #     # Perform sample calculations of entropy on scanned examples
     #     aggregate_entropy(attributes[ATTRIBUTE_NAMES[i]], EXAMPLES.shape[0])
@@ -206,11 +240,15 @@ if __name__ == '__main__':
     # print(f"Attribute count: {EXAMPLES.shape[1]}")
     # print(f"Example count: {EXAMPLES.shape[0]}")
 
-    subset = get_examples_by_type(EXAMPLES, "$$$$", attributes["Price"].col_idx)
-    print(subset)
-
-    best = choose_best_attribute(attributes, EXAMPLES)
-    print(best.name)
+    # subset = get_examples_by_type(EXAMPLES, "$$$", attributes["Price"].col_idx)
+    # print(subset)
+    #
+    # best = choose_best_attribute(attributes, EXAMPLES)
+    # print(best.name)
 
     # Build tree through "learning"
-    # root = DTL(EXAMPLES, attributes, None)
+    print(EXAMPLES[1, -1])
+    print(bool(int(EXAMPLES[1, -1])))
+
+    root = DTL(EXAMPLES, attributes, None)
+    root.print_tree()
